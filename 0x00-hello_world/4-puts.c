cf932a555edd1fecb07ade6eb2a97092de791b1c
TobiEmmanuel1 <emmanueltoby0@gmail.com>
2022-12-16 05:23:01 -0800

    the puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!bin/bash
+#include <stdio.h>
+/**
+ *  main- Serve as the entry point for the program
+ *
+ *   Return:By default returns zero if no errors otherwise returns non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiingual puzzle");
+	return (0);
+}
