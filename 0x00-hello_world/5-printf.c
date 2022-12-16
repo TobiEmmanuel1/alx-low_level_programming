326ae9cd8ed5eeb45c25f0650758d97d19b04d46
TobiEmmanuel1 <emmanueltoby0@gmail.com>
2022-12-16 09:16:21 -0800

    the push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
 /**
  *
- *  * main- Entry point for the  program
+ * main- Entry point for the  program
  *
- *   *
- *
- *    * Return: 0 if no errors, return non zero if errors
- *
- *     */
-
+ * Return: 0 if no errors, return non zero if errors
+ */
 int main(void)
-
 {
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
 }
