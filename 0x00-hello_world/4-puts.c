97304fac051b71fbdd09529f97a485ae678477be
TobiEmmanuel1 <emmanueltoby0@gmail.com>
2022-12-17 13:40:16 -0800

    the puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main- Serve as the entry point for the program
  *
- * Return: By default returns zero if no errors otherwise returns non-zero value
+ * Return:By default returns zero if no errors otherwise returns non-zero value
  */
 int main(void)
 {
